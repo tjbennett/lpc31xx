4cc77eb6810516ef0a9c9c580da050ed34ee8c8b
Jon Smirl <jonsmirl@gmail.com>
2012-05-13 12:50:13 -0400

    r2

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -37,7 +37,7 @@
 #define gadget_is_goku(g)		(!strcmp("goku_udc", (g)->name))
 #define gadget_is_imx(g)		(!strcmp("imx_udc", (g)->name))
 #define gadget_is_langwell(g)		(!strcmp("langwell_udc", (g)->name))
-#define gadget_is_lpc313x(g)		(!strcmp("lpc313x_udc", (g)->name))
+#define gadget_is_lpc31xx(g)		(!strcmp("lpc31xx_udc", (g)->name))
 #define gadget_is_m66592(g)		(!strcmp("m66592_udc", (g)->name))
 #define gadget_is_musbhdrc(g)		(!strcmp("musb-hdrc", (g)->name))
 #define gadget_is_net2272(g)		(!strcmp("net2272", (g)->name))
@@ -119,7 +119,7 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x31;
 	else if (gadget_is_dwc3(gadget))
 		return 0x32;
-	else if (gadget_is_lpc313x(gadget))
+	else if (gadget_is_lpc31xx(gadget))
 		return 0x33;
 
 	return -ENOENT;
