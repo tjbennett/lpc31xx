0dea41182337ed1b8af13c3c6bf25ff6044143d3
Jon Smirl <jonsmirl@gmail.com>
2012-09-09 14:57:56 -0400

    From 235780cba6524714ddcc79e82cc7cc73e2649269 Mon Sep 17 00:00:00 2001
    Subject: i2s-dma

--- arch/arm/mach-lpc31xx/include/mach/registers.h
+++ arch/arm/mach-lpc31xx/include/mach/registers.h
@@ -217,64 +217,6 @@
 #endif
 
 /***********************************************************************
-* Audio Subsystem (ADSS) register definitions
-**********************************************************************/
-
-/* I2S Controller Module Register Structure */
-#define I2S_FORMAT_SETTINGS       __REG (I2S_PHYS + 0x00)
-#define I2S_CFG_MUX_SETTINGS      __REG (I2S_PHYS + 0x04)
-#define I2S_CLASSED_CFG           __REG (I2S_PHYS + 0x08)
-#define I2S_CLASSED_STS           __REG (I2S_PHYS + 0x0C)
-#define I2S_N_SOF_COUNTER         __REG (I2S_PHYS + 0x10)
-
-/* I2S channels */
-#define I2S_CH_TX0 1
-#define I2S_CH_TX1 2
-#define I2S_CH_RX0 3
-#define I2S_CH_RX1 4
-
-/* I2S channel specific registers */
-#define I2S_CH_LEFT_16BIT(n)      __REG (I2S_PHYS + ((n) * 0x80) + 0x00)
-#define I2S_CH_RIGHT_16BIT(n)     __REG (I2S_PHYS + ((n) * 0x80) + 0x04)
-#define I2S_CH_LEFT_24BIT(n)      __REG (I2S_PHYS + ((n) * 0x80) + 0x08)
-#define I2S_CH_RIGHT_24BIT(n)     __REG (I2S_PHYS + ((n) * 0x80) + 0x0C)
-#define I2S_CH_INT_STATUS(n)      __REG (I2S_PHYS + ((n) * 0x80) + 0x10)
-#define I2S_CH_INT_MASK(n)        __REG (I2S_PHYS + ((n) * 0x80) + 0x14)
-#define I2S_CH_LEFT32(n)          __REG (I2S_PHYS + ((n) * 0x80) + 0x20)
-#define I2S_CH_RIGHT32(n)         __REG (I2S_PHYS + ((n) * 0x80) + 0x40)
-#define I2S_CH_INTERLEAVED(n)     __REG (I2S_PHYS + ((n) * 0x80) + 0x60)
-
-/* I2S format settings register defines */
-#define I2S_FORMAT_I2S            0x3
-#define I2S_FORMAT_LSB16          0x4
-#define I2S_FORMAT_LSB18          0x5
-#define I2S_FORMAT_LSB20          0x6
-#define I2S_FORMAT_LSB24          0x7
-#define I2S_FORMAT_MASK           0x7
-#define I2S_SET_FORMAT(n, s)      ((s) << (((n) - 1) * 3))
-
-/* I2S Mux configuration setting defines */
-#define I2S_RXO_SELECT_MASTER     _BIT(1)
-#define I2S_RX1_SELECT_MASTER     _BIT(2)
-
-/* I2S interrupt status and mask bits */
-#define I2S_FIFO_RIGHT_UNDERRUN   _BIT(0)
-#define I2S_FIFO_LEFT_UNDERRUN    _BIT(1)
-#define I2S_FIFO_RIGHT_OVERRUN    _BIT(2)
-#define I2S_FIFO_LEFT_OVERRUN     _BIT(3)
-#define I2S_FIFO_LEFT_FULL        _BIT(4)
-#define I2S_FIFO_LEFT_HALF_FULL   _BIT(5) /* RX only */
-#define I2S_FIFO_LEFT_HALF_EMPTY  _BIT(5) /* TX only */
-#define I2S_FIFO_LEFT_NOT_EMPTY   _BIT(6) /* RX only */
-#define I2S_FIFO_LEFT_EMPTY       _BIT(6) /* TX only */
-#define I2S_FIFO_RIGHT_FULL       _BIT(7)
-#define I2S_FIFO_RIGHT_HALF_FULL  _BIT(8) /* RX only */
-#define I2S_FIFO_RIGHT_HALF_EMPTY _BIT(8) /* TX only */
-#define I2S_FIFO_RIGHT_NOT_EMPTY  _BIT(9) /* RX only */
-#define I2S_FIFO_RIGHT_EMPTY      _BIT(9) /* TX only */
-#define I2S_FIFO_ALL_MASK         0x3FF
-
-/***********************************************************************
  * ADC_REG register definitions
  **********************************************************************/
 #define ADC_CON_REG            __REG (ADC_PHYS + 0x20)
